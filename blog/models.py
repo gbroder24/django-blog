10c32399fe0346f56cdfefb02bd643519c9f996f
Gary Broderick <garybroderick82@gmail.com>
2024-09-23 09:41:54 +0000

    build homepage with django generic views

--- blog/models.py
+++ blog/models.py
@@ -12,8 +12,9 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    exerpt = models.TextField(blank=True)
+    excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
 
     class Meta:
         ordering = ["-created_on"]
@@ -29,4 +30,11 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
 
