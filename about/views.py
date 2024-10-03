93a7e718303c9a804726c217420f54b95a91b8a9
Gary Broderick <garybroderick82@gmail.com>
2024-10-03 13:29:05 +0000

    handle the collaboration for POST request

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
@@ -10,6 +11,15 @@ def about_me(request):
     Renders about page
     """
 
+    if request.method == 'POST':
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid:
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
+
     about = About.objects.all().order_by('-updated_on').first()
 
     collaborate_form = CollaborateForm()
@@ -18,6 +28,6 @@ def about_me(request):
         request,
         "about/about.html",
         {"about": about,
-         "collaborate_form": collaborate_form,
+        "collaborate_form": collaborate_form,
         },
     )
